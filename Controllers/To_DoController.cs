537cf0a1523530969726fa757d8db226e49eaa44
Michael Xie <michaelx.10@hotmail.com>
2023-04-25 10:43:35 -0400

    Altered about to open Git Repo in new tab Made search mechanism working again

--- Controllers/To_DoController.cs
+++ Controllers/To_DoController.cs
@@ -8,25 +8,28 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using To_Do_List.Data;
 using To_Do_List.Models;
+using Microsoft.AspNetCore.Http;
+using Azure;
 
 namespace To_Do_List.Controllers
 {
-    public class To_DoController : Controller
-    {
-        private readonly To_Do_ListContext _context;
+	public class To_DoController : Controller
+	{
+		private readonly To_Do_ListContext _context;
 
-        public To_DoController(To_Do_ListContext context)
-        {
-            _context = context;
-        }
+		public To_DoController(To_Do_ListContext context)
+		{
+			_context = context;
+		}
 
-        // GET: To_Do
+		// GET: To_Do
 
-        // Searches Task
-        public async Task<IActionResult> Index(string sortOrder, string searchString)
-        {
-            // Switches both between ascending and descending order
-            ViewBag.TitleSortParm = sortOrder == "Title" ? "title_desc" : "Title";
+		// Searches Task
+		public async Task<IActionResult> Index(string sortOrder, string searchString, string search)
+		{
+			
+			// Switches both between ascending and descending order
+			ViewBag.TitleSortParm = sortOrder == "Title" ? "title_desc" : "Title";
 
 			ViewBag.DescriptionSortParm = sortOrder == "Description" ? "description_desc" : "Description";
 
@@ -36,40 +39,46 @@ namespace To_Do_List.Controllers
 
 
 			if (_context.To_Do == null)
-            {
-                return Problem("Entity set 'ToDo.ToDo'  is null.");
-            }
+			{
+				return Problem("Entity set 'ToDo.ToDo'  is null.");
+			}
 
-            // Creates a LINQ query to select the tasks
-            var todo = from m in _context.To_Do
-                         select m;
+			// Creates a LINQ query to select the tasks
+			var todo = from m in _context.To_Do
+					   select m;
 
-            // Orders all the parameters as needed with switches
-            switch (sortOrder)
-            {
-                case "Title":
-                    todo = todo.OrderBy(m => m.Title); //LINQ
-                    break;
+			// Checks searchString and determines if 
+			if (!String.IsNullOrEmpty(searchString) && (String.IsNullOrEmpty(search)))
+			{
+				todo = todo.Where(s => s.Title.Contains(searchString));
+			}
 
-                case "title_desc":
-                    todo = todo.OrderByDescending(m => m.Title);
-                    break;
+			// Orders all the parameters as needed with switches
+			switch (sortOrder)
+			{
+				case "Title":
+					todo = todo.OrderBy(m => m.Title); //LINQ
+					break;
 
-                case "Description":
-                    todo = todo.OrderBy(m => m.Description);
-                    break;
+				case "title_desc":
+					todo = todo.OrderByDescending(m => m.Title);
+					break;
 
-                case "description_desc":
-                    todo = todo.OrderByDescending(m => m.Description);
-                    break;
+				case "Description":
+					todo = todo.OrderBy(m => m.Description);
+					break;
 
-                case "StartDate":
-                    todo = todo.OrderBy(m => m.StartDate);
-                    break;
+				case "description_desc":
+					todo = todo.OrderByDescending(m => m.Description);
+					break;
 
-                case "startdate_desc":
-                    todo = todo.OrderByDescending(m => m.StartDate);
-                    break;
+				case "StartDate":
+					todo = todo.OrderBy(m => m.StartDate);
+					break;
+
+				case "startdate_desc":
+					todo = todo.OrderByDescending(m => m.StartDate);
+					break;
 
 				case "EndDate":
 					todo = todo.OrderBy(m => m.EndDate);
@@ -81,146 +90,141 @@ namespace To_Do_List.Controllers
 
 			}
 
-            // Function to search if 
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                todo = todo.Where(s => s.Title!.Contains(searchString));
-            }
 
-            return View(await todo.ToListAsync());
-        }
+			return View(await todo.ToListAsync());
+		}
 
-        // GET: To_Do/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.To_Do == null)
-            {
-                return NotFound();
-            }
+		// GET: To_Do/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.To_Do == null)
+			{
+				return NotFound();
+			}
 
-            var to_Do = await _context.To_Do
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (to_Do == null)
-            {
-                return NotFound();
-            }
+			var to_Do = await _context.To_Do
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (to_Do == null)
+			{
+				return NotFound();
+			}
 
-            return View(to_Do);
-        }
+			return View(to_Do);
+		}
 
-        // GET: To_Do/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: To_Do/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: To_Do/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Description,StartDate,EndDate,checkbox")] To_Do to_Do)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(to_Do);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(to_Do);
-        }
+		// POST: To_Do/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Title,Description,StartDate,EndDate,checkbox")] To_Do to_Do)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(to_Do);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(to_Do);
+		}
 
-        // GET: To_Do/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.To_Do == null)
-            {
-                return NotFound();
-            }
+		// GET: To_Do/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.To_Do == null)
+			{
+				return NotFound();
+			}
 
-            var to_Do = await _context.To_Do.FindAsync(id);
-            if (to_Do == null)
-            {
-                return NotFound();
-            }
-            return View(to_Do);
-        }
+			var to_Do = await _context.To_Do.FindAsync(id);
+			if (to_Do == null)
+			{
+				return NotFound();
+			}
+			return View(to_Do);
+		}
 
-        // POST: To_Do/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,StartDate,EndDate,checkbox")] To_Do to_Do)
-        {
-            if (id != to_Do.Id)
-            {
-                return NotFound();
-            }
+		// POST: To_Do/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,StartDate,EndDate,checkbox")] To_Do to_Do)
+		{
+			if (id != to_Do.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(to_Do);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!To_DoExists(to_Do.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(to_Do);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(to_Do);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!To_DoExists(to_Do.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(to_Do);
+		}
 
-        // GET: To_Do/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.To_Do == null)
-            {
-                return NotFound();
-            }
+		// GET: To_Do/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.To_Do == null)
+			{
+				return NotFound();
+			}
 
-            var to_Do = await _context.To_Do
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (to_Do == null)
-            {
-                return NotFound();
-            }
+			var to_Do = await _context.To_Do
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (to_Do == null)
+			{
+				return NotFound();
+			}
 
-            return View(to_Do);
-        }
+			return View(to_Do);
+		}
 
-        // POST: To_Do/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.To_Do == null)
-            {
-                return Problem("Entity set 'To_Do_ListContext.To_Do'  is null.");
-            }
-            var to_Do = await _context.To_Do.FindAsync(id);
-            if (to_Do != null)
-            {
-                _context.To_Do.Remove(to_Do);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: To_Do/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.To_Do == null)
+			{
+				return Problem("Entity set 'To_Do_ListContext.To_Do'  is null.");
+			}
+			var to_Do = await _context.To_Do.FindAsync(id);
+			if (to_Do != null)
+			{
+				_context.To_Do.Remove(to_Do);
+			}
 
-        private bool To_DoExists(int id)
-        {
-          return (_context.To_Do?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool To_DoExists(int id)
+		{
+			return (_context.To_Do?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
 }
--- Models/To Do.cs
+++ Models/To Do.cs
@@ -1,17 +1,34 @@
 ﻿using Microsoft.AspNetCore.Components.Forms;
 using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace To_Do_List.Models
 {
+
     public class To_Do
     {
         public int Id { get; set; }
+
+        // Setting requirements for attributes as shown below:
+
+        [StringLength(60, MinimumLength=3)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Title must begin with a capital letter and be a minimum 3 characters long with a maximum of 60 characters.")] // Requires first letter to be a capital letter, allows other text to be written afterwards, \s matches any whitespace characters
+        [Required] // Indicates property must have a value
         public string? Title { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Description { get; set; }
+
+        [Required]
         [Display(Name = "Start Date")]
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
 
+        [Required]
         [Display(Name = "End Date")]
         [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
